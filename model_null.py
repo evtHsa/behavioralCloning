211d0290de584bcebddb60859466d86d123241ca
eric van tassell <Eric.VanTassell@amd.com>
2018-12-23 22:03:03 -0600

    cnn, step 1: test infrastructure around empy model, wip, 1

--- model_null.py
+++ model_null.py
@@ -4,11 +4,15 @@
 #from keras.layers.pooling import MaxPooling2D
 #from keras.layers.advanced_activations import ELU
 #from keras.regularizers import l2, activity_l2
-#from keras.optimizers import Adam
 
 # FIXME: get rid of the things we don't need for this project
 
+from keras.layers.core import Dense
 from keras.models import Sequential, model_from_json
-
+from keras.optimizers import Adam
 
 null_model = Sequential()
+null_model.add(Dense(1, input_shape=(66,200,3)))
+
+#adam optimizer and mean squared error loss fn
+null_model.compile(optimizer=Adam(lr=1e-3), loss='mse')
